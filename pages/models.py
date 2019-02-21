43fe1dff76157027c72f96b1fbc858adeceb064f
Marietta Lazana <32538636+mlazana@users.noreply.github.com>
2019-02-21 17:28:53 +0200

    Initial commit

---
+++ pages/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class User(models.Model):
+    username = models.CharField(max_length=200)
+    fullName = models.CharField(max_length=200)
+    email = models.EmailField(max_length=254)
+    facebook_account = models.CharField(max_length=200)
+    instagram_account = models.CharField(max_length=200)
+    Twitter_account = models.CharField(max_length=200)
+    Github_account = models.CharField(max_length=200)
+
+
+
+
