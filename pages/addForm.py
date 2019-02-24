86c376f368804fea56c51b3fb6882f65bc6fec56
Marietta Lazana <32538636+mlazana@users.noreply.github.com>
2019-02-25 01:53:28 +0200

    Apply all columns in registration form

--- pages/addForm.py
+++ pages/addForm.py
@@ -8,7 +8,10 @@ class Add_Form(forms.Form):
 
 class RegisterForm(UserCreationForm):
 	email = forms.EmailField()
+	first_name = forms.CharField()
+	last_name = forms.CharField()
+
 
 	class Meta:
 		model = User
-		fields = ['username', 'email', 'password1', 'password2']
+		fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
