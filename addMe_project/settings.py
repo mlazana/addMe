1e0fcffae9dfbe2a795fb18ebfa44ccd3b5e64b4
Marietta Lazana <32538636+mlazana@users.noreply.github.com>
2019-02-25 02:43:20 +0200

    Login is working

--- addMe_project/settings.py
+++ addMe_project/settings.py
@@ -126,3 +126,5 @@ STATICFILES_DIRS = (
 )
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = '../index/'
--- pages/views.py
+++ pages/views.py
@@ -30,8 +30,8 @@ def register(request):
 		if form.is_valid():
 			form.save()
 			username = form.cleaned_data.get('username')
-			messages.success(request, f'Account created for { username }!')
-			return redirect('../index/')
+			messages.success(request, f'Your account has been created! You are now able to login.')
+			return redirect('login')
 	else:
 		form = RegisterForm()
 	return render(request, 'users/register.html', {'form': form})
